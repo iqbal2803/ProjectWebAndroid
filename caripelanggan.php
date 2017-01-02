4deb4b9bdff8c423a1027ffb71e0c167aa4b7789
nurularifin <chailao717@gmail.com>
2017-01-02 20:25:16 +0700

    Add files via upload
    cari data

---
+++ caripelanggan.php
@@ -0,0 +1,10 @@
+<p>Pencarian Berdasarkan
+      <select name="kategori">
+        <option value="id_pelanggan">ID</option>
+        <option value="nama_pelanggan">Nama</option>
+        <option value="alamat_pelanggan">Alamat</option>
+        <option value="no_telp">NO.Telp</option>
+      </select>
+    <input name="tcari" type="text" id="txt_cari">
+    <input name="bcari" type="submit" value="Cari">
+	</p>
