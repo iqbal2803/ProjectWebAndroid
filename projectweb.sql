3c536f11235d910e913894417747dc2a2b0f9abb
Iqbal-PC\Iqbal <m.iqbaal2803@gmail.com>
2017-01-03 00:54:25 +0700

    sql yang baru

--- projectweb.sql
+++ projectweb.sql
@@ -1,20 +1,20 @@
 -- phpMyAdmin SQL Dump
--- version 3.4.5
+-- version 4.5.1
 -- http://www.phpmyadmin.net
 --
--- Host: localhost
--- Waktu pembuatan: 02. Januari 2017 jam 07:24
--- Versi Server: 5.5.16
--- Versi PHP: 5.3.8
+-- Host: 127.0.0.1
+-- Generation Time: 02 Jan 2017 pada 18.53
+-- Versi Server: 10.1.16-MariaDB
+-- PHP Version: 5.6.24
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `projectweb`
@@ -26,24 +26,24 @@ SET time_zone = "+00:00";
 -- Struktur dari tabel `barang`
 --
 
-CREATE TABLE IF NOT EXISTS `barang` (
-  `Id_barang` char(10) DEFAULT NULL,
-  `Nama_barang` char(20) DEFAULT NULL,
-  `Harga` int(10) DEFAULT NULL,
-  `Stok` int(5) DEFAULT NULL,
-  `Gambar` varchar(15) DEFAULT NULL
+CREATE TABLE `barang` (
+  `id_barang` varchar(10) NOT NULL,
+  `nama_barang` varchar(20) DEFAULT NULL,
+  `harga` int(10) DEFAULT NULL,
+  `stok` int(5) DEFAULT NULL,
+  `gambar` varchar(15) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data untuk tabel `barang`
 --
 
-INSERT INTO `barang` (`Id_barang`, `Nama_barang`, `Harga`, `Stok`, `Gambar`) VALUES
+INSERT INTO `barang` (`id_barang`, `nama_barang`, `harga`, `stok`, `gambar`) VALUES
+('B0002', 'kamera1', 50000, 10, '1a.png'),
 ('E311', 'Nokia', 500000, 3, 'gambar'),
 ('E321', 'Nokiab', 570000, 2, 'gambarb'),
 ('E331', 'Nokiac', 575000, 3, 'gambarc'),
-('E341', 'Nokiad', 875000, 1, 'gambard'),
-('E351', 'Nokiae', 554000, 4, 'gambare');
+('E341', 'Nokiad', 875000, 1, 'gambard');
 
 -- --------------------------------------------------------
 
@@ -51,22 +51,22 @@ INSERT INTO `barang` (`Id_barang`, `Nama_barang`, `Harga`, `Stok`, `Gambar`) VAL
 -- Struktur dari tabel `pelanggan`
 --
 
-CREATE TABLE IF NOT EXISTS `pelanggan` (
-  `Id_pelanggan` char(5) DEFAULT NULL,
-  `Nama_pelanggan` char(20) DEFAULT NULL,
-  `Alamat_pelanggan` char(20) DEFAULT NULL,
-  `No_telp` int(12) DEFAULT NULL
+CREATE TABLE `pelanggan` (
+  `id_pelanggan` varchar(5) NOT NULL,
+  `nama_pelanggan` varchar(20) DEFAULT NULL,
+  `alamat_pelanggan` varchar(20) DEFAULT NULL,
+  `no_telp` int(12) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data untuk tabel `pelanggan`
 --
 
-INSERT INTO `pelanggan` (`Id_pelanggan`, `Nama_pelanggan`, `Alamat_pelanggan`, `No_telp`) VALUES
-('S999', 'asa', 'jlkamboja', 331345),
-('S998', 'isa', 'jlmawar', 331456),
+INSERT INTO `pelanggan` (`id_pelanggan`, `nama_pelanggan`, `alamat_pelanggan`, `no_telp`) VALUES
+('S996', 'ira', 'jlseruni', 331123),
 ('S997', 'ara', 'jlmelati', 331789),
-('S996', 'ira', 'jlseruni', 331123);
+('S998', 'isa', 'jlmawar', 331456),
+('S999', 'asa', 'jlkamboja', 331345);
 
 -- --------------------------------------------------------
 
@@ -74,22 +74,43 @@ INSERT INTO `pelanggan` (`Id_pelanggan`, `Nama_pelanggan`, `Alamat_pelanggan`, `
 -- Struktur dari tabel `user`
 --
 
-CREATE TABLE IF NOT EXISTS `user` (
-  `Id_user` char(5) DEFAULT NULL,
-  `Username` char(20) DEFAULT NULL,
-  `Password` char(5) DEFAULT NULL,
-  `Status` char(10) DEFAULT NULL
+CREATE TABLE `user` (
+  `id_user` varchar(5) NOT NULL,
+  `username` varchar(20) DEFAULT NULL,
+  `password` varchar(5) DEFAULT NULL,
+  `hak_akses` varchar(10) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data untuk tabel `user`
 --
 
-INSERT INTO `user` (`Id_user`, `Username`, `Password`, `Status`) VALUES
-('A', 'Rismia', '123', 'Karyawan'),
-('B', 'Sandi', '456', 'Karyawan'),
-('C', 'Shinta', '321', 'Karyawan'),
-('D', 'Dewi', '654', 'Karyawan');
+INSERT INTO `user` (`id_user`, `username`, `password`, `hak_akses`) VALUES
+('A', 'iqbal', '123', 'Admin'),
+('B', 'Nurul', '456', 'Admin'),
+('C', 'Shinta', '321', 'Karyawan');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `barang`
+--
+ALTER TABLE `barang`
+  ADD PRIMARY KEY (`id_barang`);
+
+--
+-- Indexes for table `pelanggan`
+--
+ALTER TABLE `pelanggan`
+  ADD PRIMARY KEY (`id_pelanggan`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id_user`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
